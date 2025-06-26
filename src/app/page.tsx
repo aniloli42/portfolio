22fe2e21116089a78f90648e325568f5cfe174db
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2025-06-26 00:16:02 -0500

    chore: project files
    creating files which are used to maintain the structure of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import Awards from '@/components/awards'
+import Contact from '@/components/contact'
+import Education from '@/components/education'
+import Experience from '@/components/experience'
+import Hero from '@/components/hero'
+import Projects from '@/components/projects'
+import Skills from '@/components/skills'
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Experience />
+      <Skills />
+      <Education />
+      <Awards />
+      <Contact />
+    </>
+  )
 }
